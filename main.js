9d9af3092a296d276a38b0818061018128890faa
Russ <russplaysguitar@yahoo.com>
2011-10-18 00:39:13 -0700

    Made a few adjustments, trying to get scale bar to work

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+
 $(document).ready(function(){
+  var max_width = $('.img_left').width();
+
   $('.scale a').click(function(){
     var scale_value = $(this).text();
-    var existing_width = $('.img_left').width();
-    var new_width = existing_width/scale_value;
+    var new_width = max_width/scale_value;
     $('.img_left').css('width',new_width+'px');
+  });
+  
+  $("#slider").slider({
+    change: function(event, ui) {
+      alert('ok');
+      var scale_value = $(this).value()/100;
+      var new_width = max_width/scale_value;
+      $('.img_left').css('width',new_width+'px');
+    }
   });
 });
