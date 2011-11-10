a234bf8c92fdf23383c9ad488739c53aa8703a2a
Russ <russplaysguitar@yahoo.com>
2011-11-10 13:46:43 -0800

    Added form to add new page elements

--- main.js
+++ main.js
@@ -2,18 +2,28 @@
 
 // jquery top level container
 $(document).ready(function() {
+	// create new list element
+	$content = $('<ul data-role="listview" data-inset="true" />');
+	
 	// get all pages from db
 	var pageArray = lib.query("pages");
 	
-	// get main menu content container
-	$content = $('<ul data-role="listview" data-inset="true" />');
+	// add pages to list element
 	$.each(pageArray, function(i, page){
 		$content.append('<li><a href="#pageoption">'+page.description+'</a></li>');
 	});
 	
-	// add content to page when it is crated
+	// add list element to main menu page when it is created
 	$('#mainmenu').live('pagecreate',function(event){
 		$('#mainmenu div[data-role="content"]').append($content);
+	});
+	
+	
+	$('#createPageForm').submit(function(){
+		var page = new Page();
+		page.description = $(this).find('input[name="description"]').val();
+		page.save();
+		// TODO: find a way to refresh the main menu after adding a page
 	});
 });
 
