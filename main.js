a528fab0256465e051c366fe30f0cac773f46ddf
Russ <russplaysguitar@yahoo.com>
2011-12-19 22:13:08 -0800

    Made page links work right

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // for use on jquery-mobile-test.html
 
+// gets url parameters by name
+$.urlParam = function(name){
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
+
+
 // jquery top level container
 $(document).ready(function() {
 
@@ -13,14 +20,21 @@ $(document).ready(function() {
 
 		// add pages to list element
 		$.each(pageArray, function(i, page){
-			$content.append('<li><a href="#pageoption">'+page.description+'</a></li>');
+			$item = $('<li><a href="page.html?ID='+page.ID+'">'+page.description+'</a></li>');
+			$content.append($item);
 		});		
 
 		// add content to mainmenu
 		$('#mainmenu div[data-role="content"]').append($content);
 	});
 	
-	
+	// handle viewpage display
+	$('#viewpage').live('pageshow',function(event){
+		console.log($.urlParam('ID'));
+
+		// TODO: put in dynamic code to add images here
+	});
+
 	$('#createPageForm').submit(function(){
 		var page = new Page();
 		page.description = $(this).find('input[name="description"]').val();
